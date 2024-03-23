a0a3753602347545f7350f2e55e47712529cf6e8
Guide Fari <guideg6@gmail.com>
2024-03-23 17:48:53 +0200

    feat: qa mvp

--- src/util/messages.ts
+++ src/util/messages.ts
@@ -1,10 +1,10 @@
-import { openaiApiKey } from ".."
+import { openaiApiClient } from ".."
 import type { OpenAIChatMessage } from "../types"
 
 export const formatMessage = (userInput: string): OpenAIChatMessage => ({ role: 'user', content: userInput })
 
 export const newMessage = async (history: OpenAIChatMessage[], message: OpenAIChatMessage) => {
-    const chatCompletion = await openaiApiKey.chat.completions.create({
+    const chatCompletion = await openaiApiClient.chat.completions.create({
         messages: [...history, message],
         model: 'gpt-3.5-turbo'
     })
